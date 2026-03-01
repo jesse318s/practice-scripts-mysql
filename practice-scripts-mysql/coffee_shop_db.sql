1dc06725a707ab8f1c01a4f694521e3b05d9d71e
Jesse Sites <85454439+jesse318s@users.noreply.github.com>
2026-03-01 01:49:00 -0600

    refactor for sqlite compatibility

--- practice-scripts-mysql/coffee_shop_db.sql
+++ practice-scripts-mysql/coffee_shop_db.sql
@@ -36,6 +36,6 @@ VALUES
 
 INSERT INTO Orders (OrderID, CustomerID, ProductID, Quantity, OrderDate)
 VALUES 
-(1, 1, 1, 2, NOW()),
-(2, 1, 2, 1, NOW()),
-(3, 2, 1, 1, NOW());
+(1, 1, 1, 2, CURRENT_TIMESTAMP),
+(2, 1, 2, 1, CURRENT_TIMESTAMP),
+(3, 2, 1, 1, CURRENT_TIMESTAMP);
